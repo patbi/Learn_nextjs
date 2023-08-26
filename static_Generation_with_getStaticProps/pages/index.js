fb64aea7c62c3c6d3525700774a27b8a9cdda3af
patbi <biyagapatrick@gmail.com>
2023-08-26 17:22:28 +0100

    static_Generation_with_getStaticProps

---
+++ static_Generation_with_getStaticProps/pages/index.js
@@ -0,0 +1,28 @@
+export default function UserList({ users }) {
+  return (
+    <>
+    <h1>List of users </h1> 
+    {users?.map((user) => {
+      return (
+        <div key={user.id}>
+          <p>{user.name}</p>
+          <p>{user.email}</p>
+        </div>
+      )
+    })}
+  </>
+  )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users')
+  const data = await response.json()
+  console.log(data) 
+
+  return {
+    props: {
+      users: data,
+    },
+  };
+}
+
