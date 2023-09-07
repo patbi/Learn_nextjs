e82ca35f9435d0b68c23d92bde82efddb182cdf1
patbi <biyagapatrick@gmail.com>
2023-09-07 15:35:40 +0100

    getServerSideProps_context

---
+++ getServerSideProps_context/pages/users.js
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+import User from "../components/user"
+
+export default function UserList({ users }) {
+  return (
+    <>
+    <h1>List of users </h1> 
+    <Link href="/"> Home </Link>
+    {users?.map((user) => {
+      return (
+        <div key={user.id}>
+          <User user= {user} />
+        </div>
+      )
+    })}
+  </>
+  )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users')
+  const data = await response.json()
+  console.log(data) 
+
+  return {
+    props: {
+      users: data,
+    },
+  };
+}
+
