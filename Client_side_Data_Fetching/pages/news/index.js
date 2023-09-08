5fdd9409a8a403730fcd19ab92b3c9d1f7526340
patbi <biyagapatrick@gmail.com>
2023-09-08 19:30:45 +0100

    Client_side_Data_Fetching

---
+++ Client_side_Data_Fetching/pages/news/index.js
@@ -0,0 +1,29 @@
+export default function NewsArticleList({ articles }) {
+	return (
+		<>
+			<h1>List of Articles </h1>
+			{articles && articles.map((article) => {
+				return (
+					<div key={article.id}>
+							<h2>
+								{article.id} {article.title} {article.category}
+							</h2>	
+						<hr />
+					</div>
+				)
+			})}
+		</>
+	)
+}
+
+export async function getServerSideProps() {
+	const response = await fetch('http://localhost:4000/news')
+	const data = await response.json()
+	console.log('Pre-rendering NewsArticleList')
+
+	return {
+		props: {
+			articles: data,
+		},
+	}	
+}
