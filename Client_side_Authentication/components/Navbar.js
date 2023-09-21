7cc03f7e4a80909352ab3bcedb3995e8a8b51b30
patbi <biyagapatrick@gmail.com>
2023-09-21 09:17:28 +0100

    Next-Auth Client_side_Authentication

---
+++ Client_side_Authentication/components/Navbar.js
@@ -0,0 +1,62 @@
+import Link from 'next/link'
+import { signIn, signOut, useSession } from 'next-auth/react' 
+
+export default function Navbar() {
+	const { data: session, status } = useSession()
+	const loading = status === "loading"
+	console.log({ session, loading })
+
+	return (
+		<nav className='header'>
+			<h1 className='logo'>
+				<a href='#'>NextAuth</a>
+			</h1>
+			<ul className={`main-nav ${!session && loading ? 'loading': 'loaded'}`}>
+				<li>
+					<Link href='/'>
+						Home
+					</Link>
+				</li>
+				<li>
+					<Link href='/dashboard'>
+						Dashboard
+					</Link>
+				</li>
+				<li>
+					<Link href='/blog'>
+						Blog
+					</Link>
+				</li>
+				{
+					!loading && !session && (
+					<li>
+					<Link href='/api/auth/signin'
+						onClick={e => {
+							e.preventDefault()
+							signIn('github')
+						}}
+					>
+						Sign In
+					</Link>
+					</li>
+				)}
+				
+				{
+					session && (
+					<li>
+					<Link href='/api/auth/signout'
+					  onClick={e => {
+							e.preventDefault()
+							signOut()
+						}}
+					>
+						Sign Out
+					</Link>
+					</li>
+				)}
+				
+			</ul>
+		</nav>
+
+	)
+}
---
+++ Client_side_Authentication/pages/api/hello.js
@@ -0,0 +1,5 @@
+function Hello() {
+	return <h1>Hello page</h1>
+}
+
+export default Hello
