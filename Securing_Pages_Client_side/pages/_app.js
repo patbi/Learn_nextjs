495ce823fd32c1a307e13262c9de492e2b494e66
patbi <biyagapatrick@gmail.com>
2023-09-21 10:02:52 +0100

    Next-Auth Securing_Pages_Client_side

---
+++ Securing_Pages_Client_side/pages/_app.js
@@ -0,0 +1,21 @@
+import { SessionProvider } from "next-auth/react"
+
+import Navbar from '../components/Navbar'
+import '../styles/globals.css'
+import '../components/Navbar.css'
+
+function MyApp({ 
+	Component,
+	pageProps: { session, ...pageProps },
+}) {
+	return (
+		<>
+		<SessionProvider session={session}>
+			<Navbar />
+			<Component {...pageProps} />
+		</SessionProvider>
+		</>
+	)
+}
+
+export default MyApp
