f974f9e868e75d42c1ca3bf71817aee67a85cfde
patbi <biyagapatrick@gmail.com>
2023-09-22 15:59:00 +0100

    Securing_API_Routes

---
+++ Securing_API_Routes/pages/blog.js
@@ -0,0 +1,28 @@
+import { getSession, useSession } from 'next-auth/react'
+
+export default function Blog({ infos }) {
+	const { data: session, status } = useSession()
+	const loading = status === "loading"
+	console.log({ session, loading })
+	return <h1>Blog page - {infos}</h1>
+}
+
+export async function getServerSideProps(context) {
+	const session = await getSession(context)
+
+	if(!session) {
+		return {
+			redirect: {
+				destination: `/api/auth/signin?callbackUrl=http://localhost:3000/blog`,
+				pernament: false,
+			},
+		}
+	}
+
+	return {
+		props: {
+			session,
+			infos: session ? 'List of 100 perzonalized blogs': 'List of free blogs',
+		},
+	}
+}
